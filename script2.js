7c3a76ceff733b5f650488476e500ee89b264606
AARYANZADE <zadeaaryan@gmail.com>
2026-05-06 09:38:07 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,37 @@
+//FORM
+<form>
+    <div class="mb-3>
+     <label for="exampleinputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else</div>
+         </div> 
+    <button type="submit" class="btn btn-primary"></button>
+</form>
+
+//Navbar
+<nav class="navbar navbar-expand-Ig navbar-light-by-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">MyApp</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+
+//Basic card 
+<div class="card" style="width:18rem;">
+    <img src="card-image.jpg" class="card-img.top" alt="Card image">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example to text to build on the card tiltle make up the bulk card's content</p>
+        <a href="#" class="btn btn-primary">Go Somewhere</a>
+    </div>
+</div>
